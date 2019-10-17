bfd569c500b6d4fc132fec39a598d82228b581eb
ArizShaikh <53708151+ArizShaikh@users.noreply.github.com>
2019-10-17 22:46:34 +0530

    Add files via upload

---
+++ print_prn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+//Program to print prn
+
+int main()
+{
+	long long int prn;
+	printf("Enter PRN: ");
+	scanf("%lld",&prn);
+	printf("PRN is : %lld",prn);
+	return 0;
+}
+
